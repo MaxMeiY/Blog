6f4f6fd148557d28e844bf9e8aad2af556177763
mys <meiyusheng246@163.com>
2018-11-10 17:27:50 +0800

    add user confirmation

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Config:
                                                                       'on', '1']
     MAIL_USERNAME = os.environ.get('MAIL_UERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    FLASKY_MAIL_SUBJECt_PREFIX = '<>'
+    FLASKY_MAIL_SUBJECT_PREFIX = '<>'
     FLASKY_MAIL_SENDER = 'Admin <meiyusheng246@163.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
